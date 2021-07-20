40ca32c71da2cfcbb39007c44197b2cc47931deb
akerughu <1029756481@qq.com>
2021-07-21 00:19:03 +0800

    watermark offset

--- src/main/java/com/atguigu/apitest/window/WindowTest3_EventTimeWindow.java
+++ src/main/java/com/atguigu/apitest/window/WindowTest3_EventTimeWindow.java
@@ -5,21 +5,25 @@ import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.OutputTag;
+import sun.management.Sensor;
 
 public class WindowTest3_EventTimeWindow {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        env.getConfig().setAutoWatermarkInterval(100);
 
         // socket文本流
         DataStreamSource<String> inputStream = env.socketTextStream("localhost", 7777);
 
-        // 转换成SensorReading类型
+        // 转换成SensorReading类型，分配时间戳和watermark
         DataStream<SensorReading> dataStream = inputStream.map(new MapFunction<String, SensorReading>() {
             public SensorReading map(String s) throws Exception {
                 String[] fields = s.split(",");
@@ -41,6 +45,18 @@ public class WindowTest3_EventTimeWindow {
 //                    }
 //                })
                 ;
+
+        OutputTag<SensorReading> outputTag = new OutputTag<SensorReading>("late"){};
+
+        // 基于事件时间的开窗聚合，统计15秒内温度的最小值
+        SingleOutputStreamOperator<SensorReading> minTempStream = dataStream.keyBy("id")
+                .timeWindow(Time.seconds(15))
+                .allowedLateness(Time.minutes(1))
+                .sideOutputLateData(outputTag)
+                .minBy("temperature");
+
+        minTempStream.print("minTemp");
+        minTempStream.getSideOutput(outputTag).print("late");
 
         // 执行
         env.execute();
