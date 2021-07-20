7907312c5eef7dd99c75994dacc360ca8c7d281f
akerughu <1029756481@qq.com>
2021-07-20 23:03:54 +0800

    watermark

--- src/main/java/com/atguigu/apitest/window/WindowTest3_EventTimeWindow.java
+++ src/main/java/com/atguigu/apitest/window/WindowTest3_EventTimeWindow.java
@@ -6,6 +6,9 @@ import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.windowing.time.Time;
 
 public class WindowTest3_EventTimeWindow {
     public static void main(String[] args) throws Exception {
@@ -22,7 +25,22 @@ public class WindowTest3_EventTimeWindow {
                 String[] fields = s.split(",");
                 return new SensorReading(fields[0], new Long(fields[1]), new Double(fields[2]));
             }
-        });
+        })
+                // 乱序数据设置事件时间和watermark
+                .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<SensorReading>(Time.seconds(2)) {
+            @Override
+            public long extractTimestamp(SensorReading element) {
+                return element.getTimestamp() * 1000L;
+            }
+        })
+                // 升序数据设置事件时间和watermark
+//                .assignTimestampsAndWatermarks(new AscendingTimestampExtractor<SensorReading>() {
+//                    @Override
+//                    public long extractAscendingTimestamp(SensorReading element) {
+//                        return element.getTimestamp() * 1000L;
+//                    }
+//                })
+                ;
 
         // 执行
         env.execute();
